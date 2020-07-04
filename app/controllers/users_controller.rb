a1089a3a837f6700fac4c6f65ef762afd2712dd3
Akane Oi <xiaoliuchen@outlook.jp>
2020-07-04 11:21:16 +0000

    add login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-  def show
-    @user = User.find(params[:id])    
-  end
 
   def new
-    @user = User.new    
+    @user = User.new
   end
 
   def create
